d52f1de7c6c7b34080d534534cc64b8aae872562
CspO6 <sebas172010@outlook.es>
2023-07-06 21:31:18 -0500

    trayendo los cambios

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,8 +2,9 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("recetasdb", "postgres", "backend", {
+export const sequelize = new Sequelize("recetasdb", "postgres", "12345", {
   host: "localhost",
+  logging: false,
   dialect: "postgres",
 });
 
