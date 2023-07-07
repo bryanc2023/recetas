13b05d45164cba93714100a74cd178fe2efa90b6
cesarjam02 <cesarjam02@gmail.com>
2023-07-06 21:18:58 -0500

    Cambios en el acerca, integrantes

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,9 +2,8 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("recetasdb", "postgres", "backend", {
+export const sequelize = new Sequelize("recetasdb", "postgres", "Cesarayala02", {
   host: "localhost",
   dialect: "postgres",
+  logging: false
 });
-
-
