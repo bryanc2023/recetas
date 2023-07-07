08b38d0d66cbe3f8a3e66a9433114d54f206ebf5
bryanc2023 <bryan.castelo@espoch.edu.ec>
2023-07-07 14:31:05 -0500

    TrayendoCambio

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,9 +2,10 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("recetasdb", "postgres", "backend", {
+export const sequelize = new Sequelize("recetasdb", "postgres", "sistemas123", {
   host: "localhost",
   dialect: "postgres",
+  logging: false
 });
 
 
